703a1d3d31ac66a2ad958f32783dc6b170f90243
Your Name <you@example.com>
2022-06-15 09:26:54 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeaderComp from "./components/HeaderComp.vue";
+</script>
+<template>
+  <HeaderComp />
+  <main>
+    <RouterView />
+  </main>
+</template>
+<style>
+@import "@/assets/base.css";
+@import "@/assets/style.css";
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,19 @@
+<script></script>
+<template>
+  <header class="menu-superior">
+    <div class="menu-esquerdo">
+      <span> <RouterLink to="/">Home</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/times">Marcas</RouterLink> </span>
+    </div>
+  </header>
+</template>
+<style>
+header {
+  background-color: aqua;
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+}
+</style>
